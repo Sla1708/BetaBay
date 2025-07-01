844e0f10a68218ac50cb6d6d17cc55fa8bd6feeb
Sayan Lakhoua <sayan_dev@lakhoua.com>
2025-07-01 08:47:58 -0700

    Initial Commit

---
+++ BetaBay/BetaBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaBayApp.swift
+//  BetaBay
+//
+//  Created by Sayan on 01.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetaBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
